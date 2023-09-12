df9f5203bc01f3241ffe54d2fbc37a1ee22eb711
ethoe <ryanouttrim@gmail.com>
2023-09-12 10:05:18 -0700

    ye

--- content.js
+++ content.js
@@ -1,13 +1,7 @@
-chrome.scripting.onCopy.addListener(function (info) {
-    // Check if the copied text is a Twitter link
-    if (info.selectionText && info.selectionText.startsWith("https://x.com/")) {
-        // Modify the URL
-        const modifiedLink = info.selectionText.replace(
-            "https://x.com/",
-            "https://twitter.com/"
-        );
+document.addEventListener('copy', function () {
+    navigator.clipboard.readText().then(text => {
 
-        // Write the modified link to the clipboard
-        navigator.clipboard.writeText(modifiedLink);
-    }
+        const replacedText = text.replace(/x\.com/, 'twitter.com');
+        navigator.clipboard.writeText(replacedText);
+    });
 });
